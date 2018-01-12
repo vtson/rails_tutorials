4bda1d6feb84b2e54289d12d313d42427cdb452e
vtson <vts.vuthanhson@gmail.com>
2018-01-12 16:04:21 +0700

    up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = t "layouttitle"
-    page_title.empty? ? base_title : page_title + " | " + base_title
+    @page_title = page_title
+    @base_title = t "layouttitle"
+    @page_title.empty? ? @base_title : @page_title + " | " + @base_title
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-scope "(:locale)", :locale => /en|vi/ do
-  get "static_pages/home"
+scope "(:locale)", locale: /en|vi/ do
+  get "/home", to: "static_pages#home"
 
-  get "static_pages/help"
+  get "/help", to: "static_pages#help"
 
-  get "static_pages/about"
+  get "/about", to: "static_pages#about"
 
-  get "static_pages/contact"
+  get "/contact", to: "static_pages#contact"
+
+  get "/signup", to: "users#new"
 
   root "static_pages#home"
 end
