67f8c279177b1d5e58d45add6cca9db80ea5d699
vtson <vts.vuthanhson@gmail.com>
2018-01-31 10:04:03 +0700

    Chap12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_reader :remember_token, :activation_token
+  attr_reader :remember_token, :activation_token, :reset_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   enum sex: [:unknown, :male, :female, :n_a]
@@ -64,10 +64,24 @@ class User < ApplicationRecord
     update_attributes! activated: true, activated_at: Time.zone.now
   end
 
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def create_reset_digest
+    @reset_token = User.new_token
+    update_attributes! reset_digest: User.digest(reset_token),
+      reset_sent_at: Time.zone.now
+  end
+
   def authenticated? attribute, token
     digest = send "#{attribute}_digest"
     return false unless digest
     BCrypt::Password.new(digest).is_password? token
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.email_expried.reset_password.hours.ago
   end
 
   private
